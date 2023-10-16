e3264696693c39877f54584bdab6a520a4218fbc
teegash <nategadgets@gmail.com>
2023-10-16 09:38:00 +0300

    added bottom nav

--- app/src/main/java/com/nate/mvvmlogin_nate/ui/theme/screens/bottomnavbar/BottomNavScreen.kt
+++ app/src/main/java/com/nate/mvvmlogin_nate/ui/theme/screens/bottomnavbar/BottomNavScreen.kt
@@ -1,35 +1,45 @@
 package com.nate.mvvmlogin_nate.ui.theme.screens.bottomnavbar
-//
-//import androidx.compose.foundation.layout.Arrangement
-//import androidx.compose.foundation.layout.Column
-//import androidx.compose.foundation.layout.Row
-//import androidx.compose.foundation.layout.fillMaxSize
-//import androidx.compose.foundation.layout.fillMaxWidth
-//import androidx.compose.foundation.shape.RoundedCornerShape
-//import androidx.compose.material.icons.Icons
-//import androidx.compose.material.icons.filled.Home
-//import androidx.compose.material.icons.filled.Menu
-//import androidx.compose.material.icons.filled.ShoppingCart
-//import androidx.compose.material3.BottomAppBar
-//import androidx.compose.material3.ExperimentalMaterial3Api
-//import androidx.compose.material3.FloatingActionButton
-//import androidx.compose.material3.Icon
-//import androidx.compose.material3.IconButton
-//import androidx.compose.material3.Scaffold
-//import androidx.compose.material3.Text
-//import androidx.compose.runtime.Composable
-//import androidx.compose.runtime.getValue
-//import androidx.compose.runtime.mutableStateOf
-//import androidx.compose.runtime.remember
-//import androidx.compose.runtime.setValue
-//import androidx.compose.ui.Alignment
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.draw.clip
-//import androidx.compose.ui.graphics.Color
-//import androidx.compose.ui.unit.dp
-//import com.nate.mvvmlogin_nate.navigation.ROUTE_PRODUCTS
-//import androidx.navigation.NavHostController
-//import androidx.navigation.compose.rememberNavController
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nate.mvvmlogin_nate.navigation.ROUTE_PRODUCTS
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 //
 //
 //@OptIn(ExperimentalMaterial3Api::class)
@@ -97,3 +107,60 @@ package com.nate.mvvmlogin_nate.ui.theme.screens.bottomnavbar
 ////    BottomNavBarScreen()
 ////
 ////}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomAppBar() {
+    Scaffold(
+        bottomBar = {
+            BottomAppBar( modifier = Modifier.clip(RoundedCornerShape(topEnd = 30.dp, topStart = 30.dp, bottomEnd = 5.dp, bottomStart = 5.dp)),
+                        containerColor = Color.LightGray,
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(Icons.Default.Check, contentDescription = "Localized description")
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Edit,
+                            contentDescription = "Localized description",
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Favorite,
+                            contentDescription = "Localized description",
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Person,
+                            contentDescription = "Localized description",
+                        )
+                    }
+                },
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = { /* do something */ },
+                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+                    ) {
+                        Icon(Icons.Filled.Add, "Localized description")
+                    }
+                }
+            )
+        },
+    ) { innerPadding ->
+        Text(
+            modifier = Modifier.padding(innerPadding),
+            text = ""
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PrevBottomAppBar() {
+
+    BottomAppBar()
+    
+}
